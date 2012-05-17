8f9f50e4317a4ec6df599dd131ca5b7c7cccbb4e
Ryan Olson <ryan@cray.com>
2012-05-16 19:37:41 -0500

    added some c transforms

---
+++ src/transformations/transforms.c
@@ -0,0 +1,24 @@
+#include "transforms.h"
+
+static inline swap(int i, int j)
+{
+        int tmp = i;
+        i = j;
+        j = tmp;
+}
+
+static inline void ccti_tran12(ccti_t *t) { swap(t->i[0], t->i[1]); }
+static inline void ccti_tran13(ccti_t *t) { swap(t->i[0], t->i[2]); }
+static inline void ccti_tran14(ccti_t *t) { swap(t->i[0], t->i[3]); }
+static inline void ccti_tran23(ccti_t *t) { swap(t->i[1], t->i[2]); }
+static inline void ccti_tran24(ccti_t *t) { swap(t->i[1], t->i[3]); }
+static inline void ccti_tran34(ccti_t *t) { swap(t->i[2], t->i[3]); }
+
+static inline void ccti_swap12(ccti_t *t) { ccti_tran12(t); swap(t->n[0], t->n[1]); }
+static inline void ccti_swap13(ccti_t *t) { ccti_tran13(t); swap(t->n[0], t->n[2]); }
+static inline void ccti_swap23(ccti_t *t) { ccti_tran23(t); swap(t->n[1], t->n[2]); }
+
+static inline void ccti_insi12(ccti_t *t) { ccti_swap12(t); }
+static inline void ccti_insi13(ccti_t *t) { ccti_swap13(t); }
+static inline void ccti_insi14(ccti_t *t) { ccti_swap14(t); }
+
